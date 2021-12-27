4ef96bd15225b34bbd33f689a6e64bd255ac67e5
pierrecampos <cdias.pierre@gmail.com>
2021-12-27 20:17:39 -0300

    PLayer Jump and walk animation created

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     private Rigidbody2D rig;
+    private bool isJumping;
     public float Speed;
     public float JumpForce;
+    public Animator anim;
 
     void Start() {
         rig = GetComponent<Rigidbody2D>();
@@ -16,8 +18,17 @@ public class Player : MonoBehaviour {
         rig.velocity = new Vector2(Speed, rig.velocity.y);
     }
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping) {
             rig.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+            isJumping = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.layer == 6) {
+            anim.SetBool("isJumping", false);
+            isJumping = false;
         }
     }
 }
