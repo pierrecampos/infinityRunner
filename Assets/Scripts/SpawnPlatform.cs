809cd24fa2d2d8b19dcd6593eb6f4cff8cfccae1
pierrecampos <cdias.pierre@gmail.com>
2021-12-26 23:21:39 -0300

    Recycle Method

--- Assets/Scripts/SpawnPlatform.cs
+++ Assets/Scripts/SpawnPlatform.cs
@@ -6,22 +6,40 @@ public class SpawnPlatform : MonoBehaviour {
 
     public List<GameObject> Platforms = new List<GameObject>();
 
+    private List<Transform> currentPlatforms = new List<Transform>();
+    private Transform currentPlatformPoint;
+    private Transform player;
+    private int platformIndex;
+
     public float offset;
 
     void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
         for (int i = 0; i < Platforms.Count; i++) {
-            Instantiate(Platforms[i], new Vector2(i * 30, 0f), transform.rotation);
+            Transform p = Instantiate(Platforms[i], new Vector2(i * 30, 0f), transform.rotation).transform;
+            currentPlatforms.Add(p);
             offset += 30;
         }
 
+        currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().finalPoint;
+
     }
 
-
-    public GameObject platTest;
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            Recycle(platTest);
+        float distance = player.position.x - currentPlatformPoint.position.x;
+        
+        if(distance >= 1) {
+            Recycle(currentPlatforms[platformIndex].gameObject);
+            platformIndex++;
+
+            if (platformIndex > currentPlatforms.Count - 1) {
+                platformIndex = 0;
+            }
+
+            currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platform>().finalPoint;
         }
+
     }
 
     public void Recycle(GameObject platform) {
